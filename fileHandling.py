ce66ab8e69e5a8b79e1554ab60b6823b3d9c3bea
Ibrar Ahmed <50750853+ibrarahmedsoomro@users.noreply.github.com>
2026-06-25 11:55:34 +0500

    today learn read write append readline and also delete

--- fileHandling.py
+++ fileHandling.py
@@ -1,10 +1,36 @@
-username = input("Enter a  Name:-")
-age = int(input("Enter a  Age"))
-mobile = int(input("Enter a Mobile Number")) 
 
-with open("data1.pdf", "a") as file:
-    file.write(f"Name :{username}\n")
-    file.write(f"Age:{age}\n")
-    file.write(f"Age {mobile}\n")
-    file.write("-" * 20 + "\n")
-print("Data Saved Sucessfully")
+# username = input("Enter a  Name: ")
+# age = int(input("Enter a  Age: "))
+# mobile = int(input("Enter a Mobile Number: ")) 
+
+# with open("data1.txt", "a") as file:
+#     file.write(f"Name :{username}\n")
+#     file.write(f"Age:{age}\n")
+#     file.write(f"Mobile {mobile}\n")
+#     file.write("-" * 20 + "\n")
+# print("Data Saved Sucessfully")
+
+# with open("data1.txt", "r") as file:
+#     lines = file.read()
+# print(lines)
+
+# with open("data1.txt", "w") as file:
+#     data = file.write()
+# print(data)
+
+# with open("data1.txt", "r") as file:
+#     print(file.readline())
+
+with open("data1.txt", "r") as file:
+    for line in file:
+        print(line.strip())
+
+
+import os
+
+# if os.path.exists("data1.txt"):
+#     print("File Exits")
+# else:
+#     print("File Not Exist")
+
+os.remove("data1.pdf")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+username = input("Enter a  Name: ")
+# age = int(input("Enter a  Age: "))
+# mobile = int(input("Enter a Mobile Number: ")) 
+
+# with open("data1.txt", "a") as file:
+#     file.write(f"Name :{username}\n")
+#     file.write(f"Age:{age}\n")
+#     file.write(f"Age {mobile}\n")
+#     file.write("-" * 20 + "\n")
+# print("Data Saved Sucessfully")
+
+# with open("data1.txt", "r") as file:
+#     lines = file.read()
+# print(lines)
